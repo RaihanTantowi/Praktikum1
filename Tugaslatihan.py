265b0997262a3b878c56083c1a3e553d586a88f3
RaihanTantowi <raihantantowi9@gmail.com>
2021-10-27 19:56:53 +0700

    Menambahkan file

---
+++ Tugaslatihan.py
@@ -0,0 +1,18 @@
+nama_lengkap = "Raihan Tantowi"
+nama_panggilan = "Raihan"
+npm = "312110229"
+umur = "18"
+tempat_lahir = "Jakarta"
+alamat = "Perum Telaga Murni Blok C25 N0.5, Cikarang, Kota Bekasi"
+telepon = "6289514566715"
+
+print("-"*149)
+
+txt = "Assalamu'alaikum.\n\nLet me introduce my self. My name is {}, but you can call me {}. My NPM is {}. I was born in {} and I am {} years old.\nI am very glad if you want to invite my house in {}. So, don't forget to call me before with the\nnumber {}.\n\nThank you."
+
+print(txt.format(nama_lengkap, nama_panggilan,
+      npm, tempat_lahir, umur, alamat, telepon))
+
+print("-"*149)
+
+
