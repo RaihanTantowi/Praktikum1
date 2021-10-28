68fb81951aabdd574dc986a9d7fd1376d72fe98c
RaihanTantowi <raihantantowi9@gmail.com>
2021-10-28 18:50:07 +0700

    Perubahan file

--- Tugaslatihan.py
+++ Tugaslatihan.py
@@ -7,11 +7,13 @@ tempat_lahir = input("Tempat lahir : ")
 telepon = input("Telepon : ")
 alamat = input("Alamat : ")
 
-#Menampilkan Output
+
 print("="*149)
 print("="*149)
+
 txt = "Assalamu'alaikum.\n\nLet me introduce my self. My name is {}, but you can call me {}. My NPM is {}. I was born in {} and I am {} years old. I am very glad if you want to invite my house in {}. So, don't forget to call me before with the number {}.\n\nThank you."
 
+#Menampilkan output
 print(txt.format(nama_lengkap, nama_panggilan,npm, tempat_lahir, umur, alamat, telepon))
 
 print("="*149) 
